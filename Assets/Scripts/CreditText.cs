c37f0ba45a7042a9917fe4947f3db5a6cf461f02
Dominic Ludorff <dominic.ludorff@stud.uni-hannover.de>
2023-01-20 21:56:15 +0100

    Added First impression of Credit Scene

--- Assets/Scripts/CreditText.cs
+++ Assets/Scripts/CreditText.cs
@@ -1,21 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class SceneFader : MonoBehaviour
+public class CreditText : MonoBehaviour
 {
+    float creditSpeed = 50;
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+	}
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
-        {
-			Initiate.Fade("CreditScene", Color.black, .5f);
-		}
-    }
+		transform.Translate(Vector2.up * creditSpeed * Time.deltaTime);
+	}
 }
